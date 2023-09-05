db163bc93e66798026602aa1973aeb2c8a79dcbb
JoshuU1610 <hernandezmorenojosemiguel@gmail.com>
2023-09-05 16:23:53 -0400

    Nuevas funciones, cambio del diseno y cambio de verificacion

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ window.addEventListener('scroll', e  => {
 
 taskInput.addEventListener('input', e => {
     const validation = regex.test(taskInput.value);
-    
+
     if(!validation){
         buttonAgg.disabled = true;
     } else {
@@ -64,8 +64,22 @@ myUL.addEventListener('click', e => {
 
     if(e.target.closest('.item-check')){
         const selec = e.target.closest('.item-check').parentElement.parentElement;
-        selec.classList.add('success');
-        e.target.closest('.item-check').parentElement.children[1].remove();
+        const button = e.target.closest('.item-check');
+
+        if(!selec.classList.contains('success')){
+            selec.classList.add('success');
+
+            button.innerHTML = `<svg class="svg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953l7.108-4.062A1.125 1.125 0 0121 8.688v8.123zM11.25 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953L9.567 7.71a1.125 1.125 0 011.683.977v8.123z" />
+          </svg>`;
+        } else {
+            selec.classList.remove('success');
+
+            button.innerHTML = `<svg class="svg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>`;
+        }
+
         localStorage.setItem('task', myUL.innerHTML);
     }
 
