ef76e19f2fe3138ffe22ff0c38dd31dc12f06298
JoshuU1610 <hernandezmorenojosemiguel@gmail.com>
2023-09-05 14:11:42 -0400

    cambios

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const navBar = document.querySelector('#nav-bar');
-const navContainer = document.querySelector('#nav-container');
+const taskInput = document.querySelector('#list-text');
+const buttonAgg = document.querySelector('#main-button-agg');
 
 
 window.addEventListener('scroll', e  => {
     navBar.classList.toggle('solid', window.scrollY > 0);
 })
 
+taskInput.addEventListener('input', e => {
+    if(!taskInput.value){
+        buttonAgg.disabled = true;
+    } else {
+        buttonAgg.disabled = false
+    }
+})
